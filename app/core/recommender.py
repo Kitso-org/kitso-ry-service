b0c78df8be3c1ace676dd3b3e706524cdcfa547a
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-09-14 23:34:53 -0300

    Map movie id to name

--- app/core/recommender.py
+++ app/core/recommender.py
@@ -1,11 +1,19 @@
+import io
 from surprise import KNNBaseline
 from surprise import Dataset
+from surprise import get_dataset_dir
+
+MOVIES_DATA_PATH = get_dataset_dir() + '/ml-100k/ml-100k/u.item'
+MOVIE_ID_INDEX = 0
+MOVIE_NAME_INDEX = 1
+MOVIE_RELEASE_DATE_INDEX = 2
 
 
 class Recommender:
 
     def __init__(self):
         self.train_model()
+        self.movie_id_to_name = self._load_movies_info()
 
     def train_model(self):
         # Load the movielens-100k dataset (download it if needed).
@@ -23,3 +31,13 @@ class Recommender:
 
     def get_top_n_recommended(self, user_id, n=5):
         return 'return example'
+
+    def _load_movies_info(self):
+        movie_id_to_name = {}
+        with io.open(MOVIES_DATA_PATH, 'r', encoding='ISO-8859-1') as f:
+            for movie_row in f:
+                movie_row = movie_row.split('|')
+                movie_id_str = movie_row[MOVIE_NAME_INDEX]
+                movie_name_str = movie_row[MOVIE_NAME_INDEX]
+                movie_id_to_name[movie_id_str] = movie_name_str
+        return movie_id_to_name
