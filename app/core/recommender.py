27f451b08b70d3bd51420b5117f89f660ea05928
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-09-24 00:54:32 -0300

    Deleting unused function

--- app/core/recommender.py
+++ app/core/recommender.py
@@ -9,8 +9,7 @@ from surprise import Dataset
 from surprise import get_dataset_dir
 
 FILE_PATH_RATINGS = os.path.expanduser('../data/training_data.csv')
-file_path_movies = os.path.expanduser('../data/movies_data.csv')
-file_path_users = os.path.expanduser('../data/ml-100k/u.user')
+FILE_PATH_MOVIES = os.path.expanduser('../data/movies_data.csv')
 
 
 class Recommender:
@@ -19,7 +18,6 @@ class Recommender:
         self.__load_rating_data_set()
         self.__load_movies_set()
         self.train_model()
-        self.movie_id_to_name = self._load_movies_info()
 
     def train_model(self):
         self.__load_training_set()
@@ -34,16 +32,6 @@ class Recommender:
     def get_top_n_recommended(self, user_id, n=5):
         return 'return example'
 
-    def _load_movies_info(self):
-        movie_id_to_name = {}
-        with io.open(MOVIES_DATA_PATH, 'r', encoding='ISO-8859-1') as f:
-            for movie_row in f:
-                movie_row = movie_row.split('|')
-                movie_id_str = movie_row[MOVIE_NAME_INDEX]
-                movie_name_str = movie_row[MOVIE_NAME_INDEX]
-                movie_id_to_name[movie_id_str] = movie_name_str
-        return movie_id_to_name
-
     def __load_training_set(self):
         reader = Reader(rating_scale=(1, 5))
         data = Dataset.load_from_df(
@@ -52,7 +40,7 @@ class Recommender:
 
     def __load_movies_set(self):
         self.movies_set = pd.read_csv(
-            file_path_movies, delimiter=';', encoding='latin-1')
+            FILE_PATH_MOVIES, delimiter=';', encoding='latin-1')
 
     def __load_rating_data_set(self):
         self.ratings_set = pd.read_csv(FILE_PATH_RATINGS, delimiter=';')
